a3d8f680c0e5535336ea088e64abc71d539b7787
SMaster358 <samuelmaster10@gmail.com>
2020-10-15 09:45:57 -0400

    Initial Commit

---
+++ GesturesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GesturesDemo
+//
+//  Created by Samuel Master on 10/15/20.
+//  Copyright © 2020 PogApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
